b1b3e069b552a83d555f4ec015aa464a4ca3b3f8
Tomohiko Himura <eiel.hal@gmail.com>
2017-09-15 19:55:16 +0900

    supported early bird student

--- sum.sql
+++ sum.sql
@@ -1,8 +1,9 @@
 SELECT
-  CASE WHEN c5 like '%懇親会%' then '懇親会'
+  CASE WHEN c5 like '%懇親会%' then '会場参加+懇親会'
     WHEN c5 like "%ビデオ参加%" then "ビデオ参加"
     WHEN c5 = "【スーパー早割】一般" then "会場参加"
     WHEN c5 = "【早割】一般" then "会場参加"
+    WHEN c5 = "【早割】学生" then "会場参加"
     WHEN c5 = "【スーパー早割】学生" then "会場参加"
     ELSE c5
  END as type
